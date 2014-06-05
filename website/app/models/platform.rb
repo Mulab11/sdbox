98c9a7e5de1641f02b796a1e8bb683b09d490465
陈许旻 <chen.xm.mu@gmail.com>
2014-06-06 06:55:51 +0800

    修改数据库

--- website/app/models/platform.rb
+++ website/app/models/platform.rb
@@ -2,4 +2,9 @@ class Platform < ActiveRecord::Base
   belongs_to :user
   has_many :contact_items
   #attr_accessible :type, :token, :name, :user_id
+
+  @@supp = ['test', 'renren', 'weixin']
+  def self.supported
+    @@supp
+  end
 end
--- website/config/routes.rb
+++ website/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :contacts
 
   get 'platforms/get' => 'platforms#get'
+  get 'platforms/list' => 'platforms#list'
 
   resources :platforms
 
@@ -17,7 +18,12 @@ Rails.application.routes.draw do
   
   resources :users
 
-  root 'users#main'
+  get 'receive/test' => 'platform_test#receive'
+  get 'bind/test' => 'platform_test#bind'
+  
+  get 'receive/:id' => 'main#receive'
+
+  root 'main#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
